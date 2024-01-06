138a6c5a24f288433a997192240d855541a2dc7d
yogyy <yogimr261@gmail.com>
2024-01-06 18:50:22 +0700

    fox: change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dressly/',
+  base: '/DressLy/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
