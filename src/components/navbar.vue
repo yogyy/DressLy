67c15754ade1e77880f6b7d19aa31898e78febad
yogyy <yogimr261@gmail.com>
2024-01-07 00:26:02 +0700

    fix: fix navbar icons

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,35 +1,36 @@
 <script setup lang="ts">
-import Logo from '@/components/logo.vue';
+import Logo from "@/components/logo.vue";
 const links = [
-  { name: 'Shop' },
-  { name: 'New Arrival' },
-  { name: 'Sale' },
-  { name: 'Blog' },
+  { name: "Shop" },
+  { name: "New Arrival" },
+  { name: "Sale" },
+  { name: "Blog" },
 ];
-import bag from '@/assets/bag.svg';
-import search from '@/assets/search.svg';
-import people from '@/assets/people.svg';
+import bag from "@/assets/bag.svg";
+import search from "@/assets/search.svg";
+import people from "@/assets/people.svg";
 const icons = [
-  { name: 'search', icon: search },
-  { name: 'bag', icon: bag },
-  { name: 'people', icon: people },
+  { name: "search", icon: search },
+  { name: "bag", icon: bag },
+  { name: "people", icon: people },
 ];
 </script>
 
 <template>
-  <div class="flex justify-between items-center gap-4 py-12 w-[1440xpx]">
-    <div class="flex gap-10 w-full">
+  <div class="flex w-[1440xpx] items-center justify-between gap-4 py-12">
+    <div class="flex w-full gap-10">
       <Logo />
       <ul
-        class="flex w-full gap-8 items-center justify-center flex-shrink text-[#1B1314] font-semibold">
+        class="flex w-full flex-shrink items-center justify-center gap-8 font-semibold text-[#1B1314]"
+      >
         <li v-for="link in links" :key="link.name" class="text-base font-bold">
           <a :href="'#' + link.name.toLowerCase()">{{ link.name }}</a>
         </li>
       </ul>
     </div>
-    <ul class="flex gap-6 w-fit">
-      <li v-for="icon in icons" :key="icon.name">
-        <button><img :src="icon.icon" :alt="icon.name" /></button>
+    <ul class="flex w-fit items-center justify-center gap-6">
+      <li v-for="icon in icons" :key="icon.name" class="flex items-center">
+        <button class="w-6"><img :src="icon.icon" :alt="icon.name" /></button>
       </li>
     </ul>
   </div>
