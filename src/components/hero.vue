e058d9c48dde9e4c268c2ee6d24cfdb4e03793ac
yogyy <yogimr261@gmail.com>
2024-01-07 23:57:30 +0700

    fix: change image hero

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,14 +1,20 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="py-16 flex gap-4 items-center px-0 flex-col md:flex-row-reverse">
+  <div
+    class="flex flex-col items-center gap-8 px-0 py-8 md:flex-row-reverse xl:gap-4 xl:py-16"
+  >
     <div class="flex-1">
       <img src="/img-hero.webp" alt="hero iamge" />
     </div>
-    <div class="flex-1 flex flex-col gap-10">
-      <div class="flex flex-col gap-6">
-        <h1 class="texthero title">Discover the Latest Fashion Trends</h1>
-        <p class="text-base leading-[140%] font-medium">
+    <div class="flex flex-1 flex-col gap-6 xl:gap-10">
+      <div class="flex flex-col gap-2 sm:gap-6">
+        <h1
+          class="font-serif text-[28px] font-semibold leading-[120%] text-[#12070b] xl:text-[52.37px] xl:leading-[110%]"
+        >
+          Discover the Latest Fashion Trends
+        </h1>
+        <p
+          class="text-[14px] font-medium leading-[140%] text-[#262626] sm:text-base"
+        >
           From stylish dresses to trendy tops and must-have accessories, we have
           everything you need to stay on top of the latest fashion trends. Our
           expertly curated collection features the hottest styles from top
@@ -16,22 +22,15 @@
           Shop now and discover your new favorite fashion pieces.
         </p>
       </div>
-      <button class="px-12 py-3 font-bold">Shop Now</button>
+      <button class="w-full px-12 py-3 font-bold sm:max-w-[286px]">
+        Shop Now
+      </button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.texthero.title {
-  color: #12070b;
-  font-size: 52.37px;
-  line-height: 110%;
-  font-family: 'Lora';
-  font-weight: 600;
-}
-
 button {
-  max-width: 286px;
   border-radius: 7.48px;
   line-height: 140%;
   background-color: #e09370;
